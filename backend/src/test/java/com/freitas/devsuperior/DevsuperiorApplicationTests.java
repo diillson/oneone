c99205689a3bf802474f5cb1887409ac6ca4f3ea
Edilson Freitas <diillson@hotmail.com>
2020-09-15 21:56:59 -0300

    initializing project

---
+++ backend/src/test/java/com/freitas/devsuperior/DevsuperiorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freitas.devsuperior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsuperiorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
